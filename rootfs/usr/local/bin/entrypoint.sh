8dfe828b910215d2d63b349233e8703ba5d4da98
ElevenNotes <github@11notes.ch>
2023-09-19 11:16:23 +0200

    add default config check

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/ash
   if [ -z "$1" ]; then
-    set -- "traefik" \
-      --configFile="${APP_ROOT}/etc/traefik.yaml"
+    TRAEFIK_CONFIG="${APP_ROOT}/etc/traefik.yaml"
+    if [ -f "${TRAEFIK_CONFIG}" ]; then
+      set -- "traefik" \
+        --configFile="${TRAEFIK_CONFIG}"
+    else
+      echo "${TRAEFIK_CONFIG} is missing, abort."
+      exit 1
+    fi
   fi
 
   exec "$@"
