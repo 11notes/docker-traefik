93194832a1f7e3f89e23c2e52d315dc02d3fd83f
ElevenNotes <github@11notes.ch>
2024-02-27 00:07:48 +0100

    add compose

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/ash
+  set -ex
+
   if [ -z "${1}" ]; then
-    elevenLogJSON info "starting traefik"
-    set -- "traefik" \
+    elevenLogJSON info "starting traefik with [${APP_ROOT}/etc/default.yaml]"
+    set -- traefik \
       --configFile="${APP_ROOT}/etc/default.yaml"
+  else
+    elevenLogJSON info "starting traefik with command line parameters"
+    set -- traefik "$@"
   fi
 
   exec "$@"
